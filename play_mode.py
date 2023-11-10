a6ca7fa2a3660524a9dcb53cabe3d7d0243bff41
Jung-Uiseok <tnekwjd3359@naver.com>
2023-11-10 14:24:10 +0900

    character의 handle_event 작업 완료 후 복구 및 play_mode 에 character 추가

--- play_mode.py
+++ play_mode.py
@@ -3,24 +3,31 @@ import game_framework
 
 import game_world
 from map import Map
+from character import Character
 
-def handle_event():
+def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN and SDLK_ESCAPE:
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        # else:
-        #     character.handle_event(event)
+        else:
+            character.handle_event(event)
+
 
 def init():
     global map
+    global character
 
     running = True
 
     map = Map()
     game_world.add_object(map, 0)
+
+    character = Character()
+    game_world.add_object(character, 1)
+
 
 def finish():
     game_world.clear()
