70a6bfeb7353912a94ea1b958d9723b89a63f95b
Jung-Uiseok <tnekwjd3359@naver.com>
2023-11-10 14:23:32 +0900

    맵 이미지 조정 및 위치 조정

--- map.py
+++ map.py
@@ -2,10 +2,10 @@ from pico2d import *
 
 class Map:
     def __init__(self):
-        self.image = load_image('courts.png')
+        self.image = load_image('court1.png')
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(397, 644)
+        self.image.draw(258 * 2, 242 * 2, self.image.w * 4, self.image.h * 4)
