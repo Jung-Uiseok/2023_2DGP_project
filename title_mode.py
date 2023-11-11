075abfa9cec105e8538710b09842a29b38b46b17
Jung-Uiseok <tnekwjd3359@naver.com>
2023-11-11 22:47:19 +0900

    title_mode 구현

---
+++ title_mode.py
@@ -0,0 +1,49 @@
+from pico2d import load_image, get_events, clear_canvas, update_canvas
+from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE
+
+import game_framework
+import play_mode
+
+
+def init():
+    global titleimage
+    # global startimage
+
+    titleimage = load_image('title.png')
+    # startimage = load_image('start2.png')
+
+
+def finish():
+    # global image
+    # del image
+    pass
+
+
+def update():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            game_framework.change_mode(play_mode)
+
+
+def draw():
+    clear_canvas()
+    titleimage.draw(258 * 2, 242 * 2, titleimage.w * 7, titleimage.h * 7)
+    # startimage.draw(258 * 2, 242 * 2 - 170, startimage.w * 2, startimage.h * 2)
+    update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
