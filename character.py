17418431100650a9c1f8793bbdfd27ec8e7a181d
Jung-Uiseok <tnekwjd3359@naver.com>
2023-11-11 19:53:30 +0900

    캐릭터 상하 움직임 구현

--- character.py
+++ character.py
@@ -20,6 +20,22 @@ def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 
+def up_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_UP
+
+
+def up_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_UP
+
+
+def down_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
+
+
+def down_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_DOWN
+
+
 def space_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
 
@@ -28,8 +44,8 @@ def time_out(e):
     return e[0] == 'TIME_OUT'
 
 
-PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
-RUN_SPEED_KMPH = 20.0 # Km / Hour
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -46,6 +62,10 @@ class Idle:
         if character.face_dir == -1:
             character.action = 0
         elif character.face_dir == 1:
+            character.action = 0
+        elif character.face_dir == -2:
+            character.action = 0
+        elif character.face_dir == 2:
             character.action = 0
         character.dir = 0
         character.frame = 0
@@ -65,10 +85,11 @@ class Idle:
 
     @staticmethod
     def draw(character):
-        character.image.clip_draw(int(character.frame) * 88, character.action * 88, 88, 88, character.x, character.y, 88 * 3, 88 * 3)
+        character.image.clip_draw(int(character.frame) * 88, character.action * 88, 88, 88, character.x, character.y,
+                                  88 * 3, 88 * 3)
 
 
-class Run:
+class RunLR:
 
     @staticmethod
     def enter(character, e):
@@ -90,7 +111,34 @@ class Run:
 
     @staticmethod
     def draw(character):
-        character.image.clip_draw(int(character.frame) * 88, character.action * 88, 88, 88, character.x, character.y, 88 * 3, 88 * 3)
+        character.image.clip_draw(int(character.frame) * 88, character.action * 88, 88, 88, character.x, character.y,
+                                  88 * 3, 88 * 3)
+
+
+class RunUD:
+
+    @staticmethod
+    def enter(character, e):
+        if up_down(e) or down_up(e):  # 위쪽으로 RUN
+            character.dir, character.action, character.face_dir = 2, 5, 2
+        elif down_down(e) or up_up(e):  # 아래쪽으로 RUN
+            character.dir, character.action, character.face_dir = -2, 9, -2
+
+    @staticmethod
+    def exit(character, e):
+        # if space_down(e):
+        #     character.swing()
+        pass
+
+    @staticmethod
+    def do(character):
+        character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 6
+        character.y += character.dir * RUN_SPEED_PPS * game_framework.frame_time
+
+    @staticmethod
+    def draw(character):
+        character.image.clip_draw(int(character.frame) * 88, character.action * 88, 88, 88, character.x, character.y,
+                                  88 * 3, 88 * 3)
 
 
 class StateMachine:
@@ -98,8 +146,10 @@ class StateMachine:
         self.character = character
         self.cur_state = Idle
         self.transitions = {
-            Idle : {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},
-            Run : {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle}
+            Idle: {right_down: RunLR, left_down: RunLR, right_up: Idle, left_up: Idle, up_down: RunUD, down_down: RunUD,
+                   up_up: Idle, down_up: Idle, space_down: Idle},
+            RunLR: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+            RunUD: {up_down: Idle, up_up: Idle, down_down: Idle, down_up: Idle}
         }
 
     def start(self):
